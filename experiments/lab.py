d82a15cefb04fee4f28ddf9fdc18f80c0caa34f5
Ceshine Lee <ceshine@conceptuous.com>
2011-06-20 12:54:22 +0800

    Add command-line option 'startdate' and 'enddate'

--- experiments/lab.py
+++ experiments/lab.py
@@ -2,25 +2,37 @@ import argparse
 import sys
 import pickle
 import os
+from datetime import date
 
-import warrant_test
+from warrant_test import test
 
 def main():
     parser = argparse.ArgumentParser(description='Carry out an experiment.')
-    choices = [ x for x in warrant_test.__dict__.keys() if len(x)>5 and x[:1].islower() and x!='pickle'] 
+    choices = [ x for x in test.__dict__.keys() if len(x)>5 and x[:1].islower() and x!='pickle'] 
     parser.add_argument('experiment', metavar='EX', choices=choices, help='Experiment to be carried out')
     parser.add_argument('-p', '--percent', metavar='n%', type=float, default=10, help='Index value changed in percentage')
     parser.add_argument('-d', '--data', metavar='filename', default='twii', help='The data file to be processed (without extension)')
     parser.add_argument('-m', '--mode', metavar='mode', choices=('period', 'point'), default='point', help='The way to determine if a condition has been met')
+    parser.add_argument('-s', '--start', metavar='startdate', default='1998-1-1', help='The start date')
+    parser.add_argument('-e', '--end', metavar='enddate', default='2011-6-17', help='The end date')
     
     args = parser.parse_args()
     print args
+    
+    year, month, day = map(int, args.start.split('-'))
+    startdate = date(year, month, day)
+    year, month, day = map(int, args.end.split('-'))
+    enddate = date(year, month, day)
+    lab = test(startdate, enddate)
+    
+    
     
     path = os.path.abspath(os.path.join(os.path.dirname(__file__),'../data/'))
     data = pickle.load(open(os.path.join(path,args.data+".ser")))
     
     ratio = args.percent/100
-    warrant_test.__dict__[args.experiment](data, ratio)
+    method = getattr(lab, args.experiment)
+    method(data, ratio)
     
          
 if __name__ == '__main__':
--- experiments/warrant_test.py
+++ experiments/warrant_test.py
@@ -3,57 +3,56 @@ import sys
 import os
 from datetime import date
 
-TRADE_DAYS = 50
-FROM = date(1998, 1, 1)
-TO = date(2011, 6, 19)  
-
-def init():
-    pass
-
-
-def slice_by_time(data):
-    start, end = -1, 0
-    for idx, entry in enumerate(data):
-        if start == -1 and entry[0] >= FROM:
-            start = idx
-            end = -1
-        if end == -1 and entry[0] > TO:
-            end = idx
-            break
+class test(object):
+    TRADE_DAYS = 50
+    def __init__(self, startdate, enddate):
+        self.FROM = startdate
+        self.TO = enddate
     
-    if end == -1:
-        end = len(data)
     
-    return data[start:end]
-    
-def summarize(hit):
-    count = 0
-    for item in hit:
-        if item == 'x':
-            count += 1
-    return count, float(count)/len(hit)
-
-def percent(data, ratio, mode='period'):
-    data = slice_by_time(data)
-    
-    hit = []       
-    for i in range(0, len(data)-TRADE_DAYS):
-        buyat = data[i][1]
-        flag = 'o'
-        if mode=='period':
-            for j in range(i+1, i+TRADE_DAYS):
-                if (data[j][1]-buyat)/buyat * ratio >= ratio * ratio:
-                    flag = 'x'
-                    break
-            hit.append(flag)
-        elif mode=='point':
-            if (data[i+TRADE_DAYS][1]-buyat)/buyat <= -0.1:
-                hit.append('x')
-            else:
-                hit.append('o')
-       
+    def slice_by_time(self, data):
+        start, end = -1, 0
+        for idx, entry in enumerate(data):
+            if start == -1 and entry[0] >= self.FROM:
+                start = idx
+                end = -1
+            if end == -1 and entry[0] > self.TO:
+                end = idx
+                break
         
-    print "".join(hit)
-    count, prob = summarize(hit)
-    print len(hit), count, prob
+        if end == -1:
+            end = len(data)
         
+        return data[start:end]
+        
+    def summarize(self, hit):
+        count = 0
+        for item in hit:
+            if item == 'x':
+                count += 1
+        return count, float(count)/len(hit)
+    
+    def percent(self, data, ratio, mode='period'):
+        data = self.slice_by_time(data)
+        
+        hit = []       
+        for i in range(0, len(data)-self.TRADE_DAYS):
+            buyat = data[i][1]
+            flag = 'o'
+            if mode=='period':
+                for j in range(i+1, i+self.TRADE_DAYS):
+                    if (data[j][1]-buyat)/buyat * ratio >= ratio * ratio:
+                        flag = 'x'
+                        break
+                hit.append(flag)
+            elif mode=='point':
+                if (data[i+self.TRADE_DAYS][1]-buyat)/buyat <= -0.1:
+                    hit.append('x')
+                else:
+                    hit.append('o')
+           
+            
+        print "".join(hit)
+        count, prob = self.summarize(hit)
+        print len(hit), count, prob
+            
